d100f4c0265c63923cc13000efabca21a2e88c2e
Margarita <xomrita@gmail.com>
2014-08-05 17:16:15 +0300

    placeholder, validation searchPerson.jsp navigation PersonList.jsp css text and select
    java mail

---
+++ web/js/jsForSearchPerson.js
@@ -0,0 +1,37 @@
+//document.getElementById('ageID').onkeyup = function(){
+//    alert("ok");
+//}
+document.getElementById('age').onkeyup = function(){
+    var val = document.getElementById('age').value;
+    if(isInt(val)){
+        document.getElementById('age').style.borderColor = "#ccc";
+    }
+    else{
+        document.getElementById('age').style.borderColor = "red";
+    }
+}
+document.getElementById('home').onkeyup = function(){
+    var val = document.getElementById('home').value;
+    if(isInt(val)){
+        document.getElementById('home').style.borderColor = "#ccc";
+    }
+    else{
+        document.getElementById('home').style.borderColor = "red";
+    }
+}
+document.getElementById('flat').onkeyup = function(){
+    var val = document.getElementById('flat').value;
+    if(isInt(val)){
+        document.getElementById('flat').style.borderColor = "#ccc";
+    }
+    else{
+        document.getElementById('flat').style.borderColor = "red";
+    }
+}
+function isInt(obj){
+    var reg = /^\d+$/;
+    if(reg.test(obj)){
+        return true;
+    }
+    else {return false};
+}
